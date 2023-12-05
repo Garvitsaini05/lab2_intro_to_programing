7db75b26917028c2537137577dccc3190af29fdf
Garivt Saini <garvitsaini05@gmail.com>
2023-12-05 14:11:50 -0500

    V3

--- progarm1.py
+++ progarm1.py
@@ -1,25 +1,22 @@
-print("MONTHLY TEMPERATURE RECORD")
-print("*--------------------------*")
+print("CALCULATION OF PER MONTH EXPENCES")
 print("")
-print("Enter the no.of years you want to record.!")
+print("Enter the no.of years you want to calculate.!")
 year=int(input(":- "))
 
 i=1
 total=0
 average=0
-while(i<=year*12):
-    
-    temp_per_month=int(input(f"Temperature For {i} Month :- "))
+while(i<=12):
+    monthly_expence=int(input(f"Enter month {i} Expences :- "))
 
     i=i+1
-
-    total=total+temp_per_month
-    
-
+    total=total+monthly_expence
     average=total/(year*12)
-    average_1st_year=total/(12)
+    
 
 print("------------------------------")
 
 
-print("your", year, "year average Temperature is :- ", average)
+print("your", year, "year average is :- ", average)
+
+
---
+++ program3.py
@@ -0,0 +1,7 @@
+a=0
+b="#"
+
+#nested loop for the given pattern
+
+for i in range(0,7):
+    print(b," "*(a+i),"#")
